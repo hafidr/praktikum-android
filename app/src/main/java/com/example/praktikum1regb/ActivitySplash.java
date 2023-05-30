b9bdc04048024079f4f977d997e6adbc04624225
Hafid Riyadi <hafid.riyadi17@gmail.com>
2023-05-30 12:03:27 +0700

    Add splash

--- app/src/main/java/com/example/praktikum1regb/ActivitySplash.java
+++ app/src/main/java/com/example/praktikum1regb/ActivitySplash.java
@@ -2,13 +2,26 @@ package com.example.praktikum1regb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class ActivitySplash extends AppCompatActivity {
+
+    private int waktu = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dashboard = new Intent(ActivitySplash.this, ActivityDashboard.class);
+                startActivity(dashboard);
+                finish();
+            }
+        }, waktu);
     }
 }
